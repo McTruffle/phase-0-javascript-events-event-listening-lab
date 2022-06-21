f1339ca240d374696f970b59840f26078252cf8e
McTruffle <56895893+McTruffle@users.noreply.github.com>
2022-06-21 00:47:34 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-function addingEventListener() {
-  const input = document.getElementById('input');
-input.addEventListener('click', function() {
-  alert('I was clicked!');
-});
-};
+// function addingEventListener() {
+//   const input = document.getElementById('input');
+// input.addEventListener('click', function() {
+//   alert('I was clicked!');
+// });
+// };
+
+function clickAlert() {
+  return alert('I was clicked');
+}
+
+const input = document.getElementById('input');
+input.addEventListener('click', clickAlert);
+
